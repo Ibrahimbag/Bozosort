f49f50b0a988f4908764a6e649c2a3652a0fe0a0
İbrahim Bağ <ibrahimbag33@outlook.com>
2025-02-10 13:36:09 +0300

    Moved input validation into a separate function

--- main.c
+++ main.c
@@ -2,27 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static void validate_input(int argc, char *argv[]);
+
 int main(int argc, char *argv[])
 {
-	// Check if there are any arguments
-	if (argc < 2)
-	{
-		fprintf(stderr, "Usage: %s <number1> <number2> ...\n", argv[0]);
-		return 1;
-	}
-
-	// Check if all arguments are integers
-	for (int i = 1; i < argc; i++)
-	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-				fprintf(stderr, "All arguments must be integers\n");
-				return 1;
-			}
-		}
-	}
+	// Check if arguments used correctly
+	validate_input(argc, argv);
 
 	// Convert arguments to integers and store them in an array
 	int arr_size = argc - 1;
@@ -42,4 +27,27 @@ int main(int argc, char *argv[])
 	printf("Number of swaps: %d\n", swap_count);
 
 	return 0;
+}
+
+static void validate_input(int argc, char *argv[])
+{
+	// Check if there are any arguments
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s <number1> <number2> ...\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	// Check if all arguments are integers
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				fprintf(stderr, "All arguments must be integers\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
 }
